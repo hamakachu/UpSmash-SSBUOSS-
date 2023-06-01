514e8fe1da82e200f246d34b7cda7aff865cead8
hamakachu <mentaiko0202@yahoo.co.jp>
2023-06-02 00:16:14 +0900

    処理内容の修正

--- app/javascript/edit-rule.js
+++ app/javascript/edit-rule.js
@@ -1,12 +1,12 @@
-const popup = () => {
-  const editRuleBtn = document.getElementById("edit-rule-btn")
-  const formPopup = document.getElementById("form_popup")
+// const popup = () => {
+//   const editRuleBtn = document.getElementById("edit-rule-btn")
+//   const formPopup = document.getElementById("form_popup")
   
-  editRuleBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    formPopup.setAttribute("style", "display: block;")
+//   editRuleBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     formPopup.setAttribute("style", "display: block;")
 
-  })
-}
+//   })
+// }
 
-window.addEventListener("load", popup)
+// window.addEventListener("load", popup)
