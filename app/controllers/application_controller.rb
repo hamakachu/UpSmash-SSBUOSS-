12352987a26bdda2f5a85cc10ec4fb07f01e815a
hamakachu <mentaiko0202@yahoo.co.jp>
2023-06-05 15:07:17 +0900

    ユーザーページ大枠実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   
   private 
     def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :twitter_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :twitter_id, :introduce])
     end
 end
