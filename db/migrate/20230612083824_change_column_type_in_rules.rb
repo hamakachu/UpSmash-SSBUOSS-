8722e028490eb92ec9f4a84e249b88c968deb2d6
hamakachu <mentaiko0202@yahoo.co.jp>
2023-06-12 17:54:14 +0900

    直前の修正

--- db/migrate/20230612083824_change_column_type_in_rules.rb
+++ db/migrate/20230612083824_change_column_type_in_rules.rb
@@ -1,5 +1,9 @@
 class ChangeColumnTypeInRules < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :rules, :voice_chat_id, :integer
+  end
+
+  def down
+    change_column :rules, :voice_chat_id, :string
   end
 end
